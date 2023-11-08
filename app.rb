2f207de6d9695c5044c3f12f569328de66a21c87
mshandil <146214158+mshandil@users.noreply.github.com>
2023-11-08 19:14:06 +0000

    final_code

--- app.rb
+++ app.rb
@@ -1,6 +1,42 @@
 require "sinatra"
 require "sinatra/reloader"
 
+# Define the choices for the game
+CHOICES = ["rock", "paper", "scissors"]
+
 get("/") do
-  erb(:RPS)
+  erb(:hp)
+end
+
+get("/rock") do
+  @player_choice = "rock"
+  @computer_choice = CHOICES.sample
+  @result = determine_winner(@player_choice, @computer_choice)
+  erb (:result)
+end
+
+get("/scissors") do
+  @player_choice = "scissors"
+  @computer_choice = CHOICES.sample
+  @result = determine_winner(@player_choice, @computer_choice)
+  erb (:result)
+end
+
+get("/paper") do
+  @player_choice = "paper"
+  @computer_choice = CHOICES.sample
+  @result = determine_winner(@player_choice, @computer_choice)
+  erb (:result)
+end
+
+def determine_winner(player_choice, computer_choice)
+  if player_choice == computer_choice
+    "We tied"
+  elsif (player_choice == "rock" && computer_choice == "scissors") ||
+        (player_choice == "paper" && computer_choice == "rock") ||
+        (player_choice == "scissors" && computer_choice == "paper")
+    "We won"
+  else
+    "We lost"
+  end
 end
